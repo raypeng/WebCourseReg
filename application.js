38f5c850f8b7d1b203b3a264abbd8ce51d208c19
Ray <pengrui1993@gmail.com>
2014-06-23 17:03:26 +0800

    course dropdowns by data; add/remove course; course list

---
+++ application.js
@@ -0,0 +1,133 @@
+function is_unique(arr) {
+	arr.sort();
+	for (var i = 0; i < arr.length - 1; i++) {
+		if (arr[i] == arr[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function no_conflict(course_list, session_list) {
+	// already contain duplicate courses or dummy courses
+	if (! is_unique(course_list.slice(0))) {
+		return false;
+	}
+	var times = [];
+	var code, session, title;
+	for (var i = 0; i < course_list.length; i++) {
+		code = course_list[i];
+		title = code.substring(0, 4);
+		session = session_list[i];
+		times = times.concat(json_dict[title][code]['sessions'][session]);
+	}
+	return is_unique(times);
+}
+
+function flash(text) {
+	$('.flash').html(text);			
+	$('.flash').css('visibility', 'visible');
+	$('.flash').fadeIn(function() {
+    	setTimeout(function() {
+    		$('.flash').html('Can you see me?');
+    		$('.flash').css('visibility', 'hidden');
+    	}, 2000);
+	});
+}
+
+jQuery(document).ready(function() {
+
+	var titles = Object.keys(json_dict);
+	var codes, sessions;
+	var title, code, session;
+	titles.sort();
+
+	// init empty course list
+	var course_list = [];
+	var session_list = [];
+	
+	// init course title dropdown
+	$('#course_title').find('option').remove();
+	for (var i in titles) {
+		$('#course_title').append('<option value="' + titles[i] + '">' + titles[i] + '</option>');
+	};
+
+	// init course code dropdown
+	$('#course_code').find('option').remove();
+	codes = Object.keys(json_dict[titles[0]]);
+	codes.sort();
+	for (var i in codes) {
+		$('#course_code').append('<option value="' + codes[i] + '">' + codes[i] + '</option>');
+	};
+
+	// init course session dropdown
+	$('#course_session').find('option').remove();
+	sessions = Object.keys(json_dict[titles[0]][codes[0]]['sessions']);
+	sessions.sort();
+	for (var i in sessions) {
+		$('#course_session').append('<option value="' + sessions[i] + '">' + sessions[i] + '</option>');
+	};
+
+	// update code & session dropdown upon title changes
+	$('#course_title').change(function() {
+		title = $('#course_title').val();
+		$('#course_code').find('option').remove();
+		codes = Object.keys(json_dict[title]);
+		codes.sort();
+		for (var i in codes) {
+			$('#course_code').append('<option value="' + codes[i] + '">' + codes[i] + '</option>');
+		};
+		code = $('#course_code').val();
+		$('#course_session').find('option').remove();
+		sessions = Object.keys(json_dict[title][code]['sessions']);
+		sessions.sort();
+		for (var i in sessions) {
+			$('#course_session').append('<option value="' + sessions[i] + '">' + sessions[i] + '</option>');
+		};
+	});
+
+	// update session dropdown upon code changes
+	$('#course_code').change(function() {
+		title = $('#course_title').val();
+		code = $('#course_code').val();
+		$('#course_session').find('option').remove();
+		sessions = Object.keys(json_dict[title][code]['sessions']);
+		sessions.sort();
+		for (var i in sessions) {
+			$('#course_session').append('<option value="' + sessions[i] + '">' + sessions[i] + '</option>');
+		};
+	});
+
+	// validate new course upon possible time conflict
+	$('.course_add_button').click(function() {
+		code = $('#course_code').val();
+		session = $('#course_session').val();
+		course_list.push(code);
+		session_list.push(session)
+		if (no_conflict(course_list, session_list)) {
+			// console.log(course_list);
+			// console.log(session_list);
+			$('#course_table').append('<tr><td class="course_entry_code">' + code + 
+				'</td><td class="course_entry_session">' + session + 
+				'</td><td><button class="course_delete_button">Delete</button></td></tr>');
+		} else {
+			// prompt a flash message
+			flash('Sorry, time conflict! :(');
+			console.log('time conflict');
+			course_list.pop();
+			session_list.pop();
+		}
+	});
+
+	// delete course from list via button
+	$('.course_delete_button').live('click', function() {
+		var course_to_delete = $(this).parent().parent().children('td').first().text();
+		var index = course_list.indexOf(course_to_delete);
+		course_list.splice(index, 1);
+		session_list.splice(index, 1);
+		// console.log(course_list);
+		// console.log(session_list);
+		$(this).parent().parent().remove();
+	})
+
+});
---
+++ preprocess.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import json
+import re
+from itertools import product
+
+INDEX = 'https://w5.ab.ust.hk/wcq/cgi-bin/1330/'
+
+def replace_dict(s, d):
+    for key, val in d.iteritems():
+        s = s.replace(key, str(val))
+    return s
+
+    
+def map_time(s):
+    ''' input ~ 'TuTh 09:00AM - 10:20AM'
+    '''
+    day_map = {'Mo': 1, 'Tu': 2, 'We': 3, 'Th': 4, 'Fr': 5, 'Sa': 6, 'Su': 7}
+    time_replace = {'12:20': '12:30', '12:50': '01:00',
+                    '01:20': '01:30', '01:50': '02:00',
+                    '02:20': '02:30', '02:50': '03:00',
+                    '03:20': '03:30', '03:50': '04:00',
+                    '04:20': '04:30', '04:50': '05:00',
+                    '05:20': '05:30', '05:50': '06:00',
+                    '06:20': '06:30', '06:50': '07:00',
+                    '07:20': '07:30', '07:50': '08:00',
+                    '08:20': '08:30', '08:50': '09:00',
+                    '09:20': '09:30', '09:50': '10:00',
+                    '10:20': '10:30', '10:50': '11:00',
+                    '11:20': '11:30', '11:50AM': '12:00PM'}
+    time_map = {'09:00AM': 1,  '09:30AM': 2,  '10:00AM': 3,  '10:30AM': 4, 
+                '11:00AM': 5,  '11:30AM': 6,  '12:00PM': 7,  '12:30PM': 8, 
+                '01:00PM': 9,  '01:30PM': 10, '02:00PM': 11, '02:30PM': 12,
+                '03:00PM': 13, '03:30PM': 14, '04:00PM': 15, '04:30PM': 16, 
+                '05:00PM': 17, '05:30PM': 18, '06:00PM': 19, '06:30PM': 20, 
+                '07:00PM': 21, '07:30PM': 22, '08:00PM': 23, '08:30PM': 24,
+                '09:00PM': 25, '09:30PM': 26, '10:00PM': 27, '10:30PM': 28,
+                '11:00PM': 29, '11:30PM': 30}
+    day, time = tuple(s.split(' ', 1))
+    day_list = [int(char) for char in replace_dict(day, day_map)]
+    time_str = replace_dict(replace_dict(time, time_replace), time_map)
+    lower, upper = tuple(map(int, time_str.split(' - ')))
+    time = [map(lambda x: x + 100 * d, range(lower, upper)) for d in day_list]
+    return [tt for t in time for tt in t]
+
+    
+def parse_datetime(str_list):
+    ''' input ~ ['L1 (3091)</td>\n<td>TuTh 09:00AM - 10:20AM',
+                 'T1A (3093)</td>\n<td>Th 06:00PM - 06:50PM',
+                 'T1B (3095)</td>\n<td>Fr 06:00PM - 06:50PM']
+        output ~ {'tut': {}, 'lab': {'LA1': [406, 407, 408, 409]}, 
+                 'credit': 4.0, 'lec': {'L1': [216, 217, 218, 416,
+                 417, 418]}}
+    '''
+    lec, tut, lab = {}, {}, {}
+    for s in str_list:
+        if '<br>' in s:
+            time_raw = re.search(r'(?<=<br>).*', s).group()
+        else:            
+            time_raw = re.search(r'(?<=<td>).*', s).group()
+        if time_raw == 'TBA':
+            continue
+        time = map_time(time_raw)
+        session = re.match(r'.*?(?= )', s).group()
+        if 'R' in session:
+            continue
+        elif 'LA' in session:
+            lab[session] = time
+        elif 'T' in session:
+            tut[session] = time
+        else:
+            lec[session] = time
+    
+    return {'lec': lec, 'tut': tut, 'lab': lab}
+           
+
+def parse_str(s):
+    ''' output ~ {'code': 'PHYS 1112', 'title': 'General Physics I 
+                   with Calculus', 'tut': {'T2D': [302, 303], 'T1A': 
+                   [211, 212], 'T1C': [214, 215], 'T1B': [211, 212], 
+                   'T1D': [214, 215], 'T2B': [508, 509], 'T2C': [302, 
+                   303], 'T3C': [311, 312], 'T3B': [219, 220], 'T3A': 
+                   [219, 220], 'T3D': [311, 312], 'T2A': [508, 509]}, 
+                   'lab': {}, 'credit': 3.0, 'lec': {'L1': [201, 202,
+                   203, 401, 402, 403], 'L3': [316, 317, 318, 516, 
+                   517, 518], 'L2': [310, 311, 312, 510, 511, 512]}, 
+                   'matching': 1}
+    '''
+    intro = re.search(r'(?<=<h2>)(?P<code>.*?) - (?P<title>.*) ' \
+               + r'\((?P<credits>[\d+.]*\d+) unit[s]*\)(?=</h2>)', s)
+    info = intro.groupdict()
+    info['credits'] = float(info['credits'])
+    info['title'] = info['title'].replace("'", ' ')
+    if '<div class="matching">' in s:
+        info['matching'] = 1
+    else:
+        info['matching'] = 0
+    temp = re.findall(r'(?<=">).*</td>\n<td>.*?(?=</td>)', s)
+    datetime = parse_datetime(temp)
+    return dict(info.items() + datetime.items())
+    
+
+def parse_url(INDEX_URL):
+    response = urllib2.urlopen(INDEX_URL)
+    html = response.read()
+    depts_match = re.search(r'(?<=<div class="depts">).*(?=</div>)', html)
+    depts = re.findall(r'(?<=">)\w{4}(?=</a>)', depts_match.group())
+    catalog = {}
+    for dept in depts:
+        print 'reading course titles', dept
+        dept_url = INDEX_URL + 'subject/' + dept
+        response = urllib2.urlopen(dept_url)
+        html = response.read()
+        data = html.split('<script type="text/javascript">')
+        data = data[1]
+        courses = data.split('<div class="course">')
+        courses = courses[1:]
+        for course in courses:
+            info = parse_str(course)
+            info = {info['code'][:4]: {info['code']: info}}
+            for k, v in info.items():
+                if k in catalog:
+                    catalog[k].update(v)
+                else:
+                    catalog.update(info)
+    return catalog
+
+
+def is_match(s):
+    ''' input ~ 'L1T1A' -> True or 'L1T2B' -> False
+    '''
+    nums = re.findall(r'\d+', s)
+    return len(set(map(int, nums))) == 1
+
+
+def get_sessions(course_info):
+    sessions = {}
+    c = course_info.copy()
+    if not c['lec']:
+        c['lec'] = {'': [-1]}
+    if not c['tut']:
+        c['tut'] = {'': [-1]}
+    if not c['lab']:
+        c['lab'] = {'': [-1]}
+    for (lec, tut, lab) in product(c['lec'], c['tut'], c['lab']):
+        key = ''.join([lec, tut, lab])
+        val = sorted([e for e in c['lec'][lec] + c['tut'][tut] + c['lab'][lab]
+               if e > 0])
+        if len(val) == len(set(val)):
+            sessions[key] = val
+    if course_info['matching'] == 0:
+        course_info.update({'sessions': sessions})
+        return course_info
+    else:
+        s = {}
+        for k, v in sessions.iteritems():
+            if is_match(k):
+                s[k] = v
+        course_info.update({'sessions': s})
+        return course_info
+                    
+            
+def get_json(INDEX_URL):
+    catalog = parse_url(INDEX_URL)
+    for title in catalog:
+        for course in catalog[title]:
+            catalog[title][course] = get_sessions(catalog[title][course])
+    return catalog
+
+
+def save_json(catalog, outfile):
+    with open(outfile, 'w') as f:
+        json.dump(catalog, f, encoding='utf-8')
+
+
+def load_json(infile):
+    with open(infile, 'r') as f:
+        return json.load(f, encoding='utf-8')
+
+
+def json2js(infile, outfile):
+    with open(infile, 'r') as inf:
+        with open(outfile, 'w') as outf:
+            s = inf.read()
+            s = "var json_text = '" + s + \
+                "';\nvar json_dict = JSON.parse(json_text);"
+            outf.write(s)
+
+            
+catalog = get_json(INDEX)
+save_json(catalog, 'new.json')
+json2js('new.json', 'data.js')
